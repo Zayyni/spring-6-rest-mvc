99604bc976a397eea72bea25a816b56702d8e838
ZAYYNI <zainulislam.733@gmail.com>
2023-03-30 22:56:48 +0500

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/zayyni8/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/zayyni8/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,8 +2,10 @@ package com.zayyni8.spring6restmvc.repositories;
 
 import com.zayyni8.spring6restmvc.entities.Beer;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.zayyni8.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,6 +20,21 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong() {
+
+        assertThrows(ConstraintViolationException.class,()->{
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("2345342453786")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
+
+    @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
@@ -29,7 +46,5 @@ class BeerRepositoryTest {
         beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
-
-
     }
 }
